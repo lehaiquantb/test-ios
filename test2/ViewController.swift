e05ed41010bb16d4e54e6c206771709e12784687
Quan Le <quanlh@Quans-Mac-mini.local>
2021-12-14 18:23:20 +0700

    initial

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Quan Le on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
